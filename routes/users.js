2adec6d92d57524675088b4ef1d2817f55dda7df
nwaamadioha <udokwu.temple960@gmail.com>
2022-06-06 18:26:41 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { updateUser, updateUserPassword, deleteUser, getUser, getUsers } from "../controllers/users.js"
-import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+// import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //UPDATE BY ADMIN
-router.put("/:id", verifyAdmin, updateUser)
+router.put("/:id", updateUser)
 
 //UPDATE PASSWORD BY USER
-router.put("/password/:id", verifyUser, updateUserPassword)
+router.put("/password/:id", updateUserPassword)
 
 //DELETE
-router.delete("/:id", verifyAdmin, deleteUser)
+router.delete("/:id", deleteUser)
 
 //GET
 router.get("/:id",  getUser)
